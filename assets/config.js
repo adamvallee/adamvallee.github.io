bf3bafa7413edcb0aef89b88df9b984092bbf045
adamvallee <26856345+adamvallee@users.noreply.github.com>
2022-05-06 22:27:22 -0400

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,23 +1,23 @@
 let config = {
-  "Title": "Your Name - Links",
-  "Name": "Your Name",
-  "Description": "Your Description",
+  "Title": "Adam David Vallee - Links",
+  "Name": "Adam David Vallee",
+  "Description": "My Links",
   "links": [
     {
       "Title": "Website",
-      "URL": "http://www.harindu.dev",
+      "URL": "http://www.adamdavid.ca",
       "icon_classes": "fas fa-link",
     },
     {
       "Title": "Github",
-      "URL": "https://github.com/harindulk",
+      "URL": "https://github.com/adamvallee",
       "icon_classes": "fab fa-github",
     },
-    {
-      "Title": "Play Store",
-      "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
-      "icon_classes": "fab fa-google-play"
-    },
+    #{
+    #  "Title": "Play Store",
+    #  "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
+    #  "icon_classes": "fab fa-google-play"
+    #},
     {
       "Title": "PC Games",
       "URL": "https://harindulk.itch.io/",
@@ -30,17 +30,17 @@ let config = {
     },
     {
       "Title": "Twitter",
-      "URL": "https://twitter.com/Harindu_Fonseka",
+      "URL": "https://twitter.com/avallee",
       "icon_classes": "fab fa-twitter"
     },
     {
       "Title": "LinkedIn",
-      "URL": "https://www.linkedin.com/in/harindulk/",
+      "URL": "https://www.linkedin.com/in/adamvallee/",
       "icon_classes": "fab fa-linkedin"
     },
     {
       "Title": "Youtube",
-      "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
+      "URL": "https://www.youtube.com/channel/UC4wqi8QDc0dm7JDe1xZ5WoA",
       "icon_classes": "fab fa-youtube"
     },
   ]
